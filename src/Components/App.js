8a3fde778a1f673c3ab7eedd8ebbb465e91dabb8
Norbert Parraghy <parraghy.norbert@ingatlan.com>
2018-06-26 11:37:42 +0200

    adds container class to the parent div

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,8 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Form />
+      <div className="App row">
+	<div class="col s12">
+	    <Form />
+	</div>
       </div>
     );
   }
